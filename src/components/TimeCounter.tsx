318d02124ea9b1869bea3f976dc46fd2b478252e
Lisbons <jeremy03hidalgo@gmail.com>
2026-05-07 14:57:59 +0200

    Añadiendo textos del Heroa

--- src/components/TimeCounter.tsx
+++ src/components/TimeCounter.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import loveConfig from '@/loveConfig';
+import loveConfig from '@/config/loveConfig';
 import styles from './TimeCounter.module.css';
 
 interface TimeLeft {
